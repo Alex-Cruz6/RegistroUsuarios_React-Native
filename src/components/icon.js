73a16107edfed97a53063969d2ce2c0e2052aaae
FloresCnzls <85141586+FloresCnzls@users.noreply.github.com>
2024-11-24 21:49:33 -0600

    Created Icons.js

---
+++ src/components/icon.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from '../styles/globalStyles';
+
+const Icon = () => {
+  return (
+    <View style={styles.icon}>
+      <Text style={styles.iconText}>👤</Text>
+    </View>
+  );
+};
+
+export default Icon;
